919863f8ac0dae146581a09786b1854e6daf1d9b
SisiYin <t3chyi00@students.oamk.fi>
2024-11-12 22:50:07 +0200

    Favorite

--- src/components/FavoriteMovies.js
+++ src/components/FavoriteMovies.js
@@ -2,17 +2,16 @@ import React, { useState, useEffect } from 'react';
 import MovieList from './MovieList';
 
 const FavoriteMovies= () => {
-  const [movies, setMovies] = useState([]);
-  // const [type, setType] = useState('popular'); 
+  const [movies, setMovies] = useState([]); 
 
   // fetch data
-  const fetchMovies = async () => {
-    const url = "https://api.themoviedb.org/3/account/21613810/favorite/movies";
+  const fetchFavoriteMovies = async () => {
+    const url = "https://api.themoviedb.org/3/account/21613810/favorite/movies?api_key=54c539f0a2dca863d152652c08d28924&session_id=df14e615c6e8a37fc3396968bdc758d8c1e051a0";
     const options = {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NGM1MzlmMGEyZGNhODYzZDE1MjY1MmMwOGQyODkyNCIsIm5iZiI6MTczMDkzMzU5Ny4wNDU3NjksInN1YiI6IjY3MmIwY2VlMmY2NGViZThjOGU0ZGVmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6pw2lCXSmmNW75P1F8EVCq-dMxYpyPwn2QcF3f7PV7Y', 
+        // Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NGM1MzlmMGEyZGNhODYzZDE1MjY1MmMwOGQyODkyNCIsIm5iZiI6MTczMDkzMzU5Ny4wNDU3NjksInN1YiI6IjY3MmIwY2VlMmY2NGViZThjOGU0ZGVmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6pw2lCXSmmNW75P1F8EVCq-dMxYpyPwn2QcF3f7PV7Y', 
       },
     };
 
@@ -27,19 +26,12 @@ const FavoriteMovies= () => {
 
   
   useEffect(() => {
-    fetchMovies();
+    fetchFavoriteMovies();
   });
 
   return (
     <div>
-      
-      {/* <div>
-        <button onClick={() => setType('top_rated')}>Top Rated</button>
-        <button onClick={() => setType('upcoming')}>Upcoming</button>
-        <button onClick={() => setType('popular')}>Most Popular</button>
-      </div>  */}
-
-     
+    
       <MovieList movies={movies} />
     </div>
   );
